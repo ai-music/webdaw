9fae84f959db8a5f143514ed25fef2c945045c46
Hans-Martin Will <hwill@acm.org>
2023-12-17 22:15:17 -0800

    Fix number of digits for ms in time displays

--- src/ui/Time.tsx
+++ src/ui/Time.tsx
@@ -10,7 +10,7 @@ function formatTime(timestamp: number): string {
   const hours = date.getUTCHours().toString().padStart(2, '0');
   const minutes = date.getUTCMinutes().toString().padStart(2, '0');
   const seconds = date.getUTCSeconds().toString().padStart(2, '0');
-  const milliseconds = date.getUTCMilliseconds().toString().padStart(4, '0');
+  const milliseconds = date.getUTCMilliseconds().toString().padStart(3, '0');
 
   return `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
