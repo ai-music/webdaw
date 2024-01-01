0ac6a02bf99b2da78298f945e67331ca8af4a95c
Hans-Martin Will <hwill@acm.org>
2024-01-01 12:00:56 -0800

    Adding reference to design note

--- src/ui/Config.ts
+++ src/ui/Config.ts
@@ -11,6 +11,6 @@ export const TIMELINE_FACTOR_PX = 16;
 /**
  * Dimensions of a scroll bar. This is really browser-specific, but for now we are simply hardwiring those values.
  * May may investigate a more robust solution later, which may rely on browser-specific CSS extensions (such as
- * `::-webkit-scrollbar).
+ * `::-webkit-scrollbar). See also the design note on scrolling.
  */
 export const SCROLLBAR_DIMENSIONS_PX = 15;
